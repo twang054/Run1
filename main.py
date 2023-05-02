44c5ead0b92384a1fd57c3d8ffca6409d6487a87
twang054 <thomas_j_wang@brown.edu>
2023-05-01 20:18:18 -0400

    HSFDFF

---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame  
+import sys  
+
+from player import Player
+from wall import Wall
+
+pygame.init()  
+# Initializations
+clock = pygame.time.Clock()  
+running = True
+fps = 60  
+background = [255, 255, 255]
+pygame.display.set_caption('Run')  
+image = pygame.image.load("run_background.webp")  
+size =[1280, 720]  
+screen = pygame.display.set_mode(size)  
+
+# Load Player
+player = Player()
+player.position.x, player.position.y = 640, 500
+
+# Define keys for player movement  
+player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_SPACE]  
+player
+
+wall = Wall([100,100])  # The other sprites
+
+wall_group = pygame.sprite.Group()  # This is how we do collision detections. We'll have to group a bunch of walls
+wall_group.add(wall)  # We'll also have to make another class for walls
+
+player_group = pygame.sprite.Group()  
+player_group.add(player)  
+
+while running:  
+    dt = clock.tick(60) * .001 * fps
+    for event in pygame.event.get():  # Allows quitting with the red button
+        if event.type == pygame.QUIT:  
+            running = False 
+            # Horizontal Movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.LEFT_KEY = True
+            elif event.key == pygame.K_RIGHT:
+                player.RIGHT_KEY = True
+            elif event.key == pygame.K_SPACE:
+                player.jump()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.LEFT_KEY = False
+            elif event.key == pygame.K_RIGHT:
+                player.RIGHT_KEY = False
+            elif event.key == pygame.K_SPACE:
+                if player.is_jumping:
+                    player.velocity.y *= .25 # quarters upwards velocity when space is let go
+                    player.is_jumping = False
+
+    # Update Sprite
+    player.update(dt)
+
+    # Update Screen
+    screen.fill(background)
+    screen.blit(image,(0, 0))  
+    player.draw(screen)
+    # first parameter takes a single sprite  
+    # second parameter takes sprite groups  
+    # third parameter is a kill command if true  
+    hit = pygame.sprite.spritecollide(player, wall_group, True)  
+    if hit:  
+    # if collision is detected call a function to destroy  
+        # rect  
+        player.image.fill((255, 255, 255))  
+    player_group.draw(screen)  
+    wall_group.draw(screen)  
+    pygame.display.update()  
+    clock.tick(fps)  
+pygame.quit()  
+sys.exit  
---
+++ player.py
@@ -0,0 +1,50 @@
+import pygame
+sprite_size = (100,100)
+horizontal_acceleration = 5
+ground_y = 600
+velocity_cap = 15
+class Player(pygame.sprite.Sprite):  
+    def __init__(self):  
+        pygame.sprite.Sprite.__init__(self)  
+        self.image = pygame.Surface(sprite_size)  # Sets the size of the sprite
+        self.image.fill((255, 0, 255))  # Sets the color of the sprite
+        self.rect = self.image.get_rect()  
+        self.LEFT_KEY, self.RIGHT_KEY= False, False
+        self.is_jumping, self.on_ground, self.friction = False, True, False
+        self.gravity, self.friction = .35, -.12
+        self.position, self.velocity = pygame.math.Vector2(0,0), pygame.math.Vector2(0,0)
+        self.acceleration = pygame.math.Vector2(0,self.gravity)
+    
+    def draw(self, display):
+        display.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def update(self, dt):
+        self.horizontal_movement(dt)
+        self.vertical_movement(dt)
+    
+    def horizontal_movement(self, dt):
+        self.acceleration.x = 0
+        if self.LEFT_KEY:
+            self.acceleration.x -= horizontal_acceleration
+        elif self.RIGHT_KEY:
+            self.acceleration.x += horizontal_acceleration
+        self.acceleration.x += self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x * dt
+        self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
+        self.rect.x = self.position.x
+    
+    def vertical_movement(self,dt):
+        self.velocity.y += self.acceleration.y * dt
+        if self.velocity.y > velocity_cap: self.velocity.y = velocity_cap
+        self.position.y += self.velocity.y * dt + (self.acceleration.y * .5) * (dt * dt)
+        if self.position.y > ground_y:
+            self.on_ground = True
+            self.velocity.y = 0
+            self.position.y = ground_y
+        self.rect.bottom = self.position.y
+    
+    def jump(self):
+        if self.on_ground:
+            self.is_jumping = True
+            self.velocity.y -= 12
+            self.on_ground = False
