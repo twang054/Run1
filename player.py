7bf9add74ebf298314445b72f357db9104913885
mliuall <matthew_t_liu@brown.edu>
2023-05-11 22:38:17 -0400

    got the background to move

--- player.py
+++ player.py
@@ -9,21 +9,25 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):  
         pygame.sprite.Sprite.__init__(self)  
         self.image = spritesheet.get_sprite(0,0,64,64)
-        self.image = pygame.transform.scale(self.image, (50,50))
+        self.image = pygame.transform.scale(self.image, (25,25))
         self.rect = self.image.get_rect()
         self.LEFT_KEY, self.RIGHT_KEY= False, False
         self.is_jumping, self.on_ground, self.friction = False, True, False
-        self.gravity, self.friction = .5, -.12
+        self.gravity, self.friction = 1, -.5
         self.position, self.velocity = pygame.math.Vector2(0,0), pygame.math.Vector2(0,0)
         self.acceleration = pygame.math.Vector2(0,self.gravity)
-        
+        self.ground_y = 400
+        self.left_border, self.right_border = 250, 11500
+
     
     def draw(self, display):
         display.blit(self.image, (self.rect.x, self.rect.y))
     
-    def update(self, dt):
+    def update(self, dt, tiles):
         self.horizontal_movement(dt)
+        self.checkCollisionsx(tiles)
         self.vertical_movement(dt)
+        self.checkCollisionsy(tiles)
     
     def horizontal_movement(self, dt):
         self.acceleration.x = 0
@@ -35,7 +39,7 @@ class Player(pygame.sprite.Sprite):
         self.velocity.x += self.acceleration.x * dt
         self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
         self.rect.x = self.position.x
-    
+
     def vertical_movement(self,dt):
         self.velocity.y += self.acceleration.y * dt
         if self.velocity.y > velocity_cap: self.velocity.y = velocity_cap
@@ -49,5 +53,38 @@ class Player(pygame.sprite.Sprite):
     def jump(self):
         if self.on_ground:
             self.is_jumping = True
-            self.velocity.y -= 12
+            self.velocity.y -= 20
             self.on_ground = False
+
+    def get_hits(self, tiles):
+        hits = []
+        for tile in tiles:
+            if self.rect.colliderect(tile):
+                hits.append(tile)
+        return hits
+
+    def checkCollisionsx(self, tiles):
+        collisions = self.get_hits(tiles)
+        for tile in collisions:
+            if tile == '' > 0:  # Hit tile moving right
+                self.position.x = tile.rect.left - self.rect.w
+                self.rect.x = self.position.x
+            elif self.velocity.x < 0:  # Hit tile moving left
+                self.position.x = tile.rect.right
+                self.rect.x = self.position.x
+
+    def checkCollisionsy(self, tiles):
+        self.on_ground = False
+        self.rect.bottom += 1
+        collisions = self.get_hits(tiles)
+        for tile in collisions:
+            if self.velocity.y > 0:  # Hit tile from the top
+                self.on_ground = True
+                self.is_jumping = False
+                self.velocity.y = 0
+                self.position.y = tile.rect.top
+                self.rect.bottom = self.position.y
+            elif self.velocity.y < 0:  # Hit tile from the bottom
+                self.velocity.y = 0
+                self.position.y = tile.rect.bottom + self.rect.h
+                self.rect.bottom = self.position.y
